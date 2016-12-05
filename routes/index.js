c9cc94ccc119eb1e5dd13f26c9fe8f79ffb73ec3
gudivadarahul <gudivadarahul@gmail.com>
2016-12-04 18:58:44 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+var request = require('request');
+
+var env = {
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { env: env });
+});
+
+router.get('https://diymatic.auth0.com/login?client=FkbXOKgCeZPYZnERHvxeo3DsziSXlKf2',
+  function(req, res){
+    res.render('login', { env: env });
+  });
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/user', ensureLoggedIn, function(req, res, next) {
+  res.render('user', { user: req.user });
+});
+
+router.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/user');
+  });
+
+module.exports = router;
